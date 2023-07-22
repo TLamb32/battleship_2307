b6fa693ad72a426f74a840a71bddbcfb3e1c6c41
Terry Lamb <terrylamb@gmail.com>
2023-07-22 16:31:26 -0400

    write tests for #consecutive_check helper method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -44,13 +44,14 @@ RSpec.describe Board do
       expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A3"])).to eq(true)
       expect(@board.valid_placement?(@submarine, ["A2", "A3"])).to eq(true)
     end
+  end
 
-    xit "can make sure the coordinates are consecutive" do
-      expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A4"])).to eq(false)
-      expect(@board.valid_placement?(@submarine, ["A1", "C1"])).to eq(false)
-      expect(@board.valid_placement?(@cruiser, ["A3", "A2", "A1"])).to eq(false)
-      expect(@board.valid_placement?(@submarine, ["C1", "B1"])).to eq(false)
-
+  describe "#consecutive_check" do
+    it "can make sure the coordinates are consecutive" do
+    expect(@board.consecutive_check(@cruiser, ["A1", "A2", "A4"])).to eq(false)
+    expect(@board.consecutive_check(@submarine, ["A1", "C1"])).to eq(false)
+    expect(@board.consecutive_check(@cruiser, ["A1", "A2", "A3"])).to eq(true)
+    expect(@board.consecutive_check(@submarine, ["B1", "C1"])).to eq(true)
     end
   end
 
