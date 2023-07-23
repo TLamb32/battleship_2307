4320c39af6c59d6ac9d64038d3fe7f7e999becdb
Terry Lamb <terrylamb@gmail.com>
2023-07-23 13:05:39 -0400

    feat: make #render method with both option = false and option = true

--- lib/board.rb
+++ lib/board.rb
@@ -107,18 +107,12 @@ class Board
   end
   
   def render(option = false)
-    # if option == false  
-    #   a = "  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n"
-    # else option == true
-    #   b = "  1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n"
-    # end
-    
-    rows = @cells.each_slice(4) do |cell_1|
-      # row_1 = []
-      # cell_1.each do |cell|
-      #   row_1 << cell.render
+    if option == false  
+      # require 'pry'; binding.pry
+      a = "  1 2 3 4 \n" + "A #{@cells["A1"].render} #{@cells["A2"].render} #{@cells["A3"].render} #{@cells["A4"].render} \n" + "B #{@cells["B1"].render} #{@cells["B2"].render} #{@cells["B3"].render} #{@cells["B4"].render} \n" + "C #{@cells["C1"].render} #{@cells["C2"].render} #{@cells["C3"].render} #{@cells["C4"].render} \n" + "D #{@cells["D1"].render} #{@cells["D2"].render} #{@cells["D3"].render} #{@cells["D4"].render} \n"
+    else option == true
+      "  1 2 3 4 \n" + "A #{@cells["A1"].render(true)} #{@cells["A2"].render(true)} #{@cells["A3"].render(true)} #{@cells["A4"].render(true)} \n" + "B #{@cells["B1"].render(true)} #{@cells["B2"].render(true)} #{@cells["B3"].render(true)} #{@cells["B4"].render(true)} \n" + "C #{@cells["C1"].render(true)} #{@cells["C2"].render(true)} #{@cells["C3"].render(true)} #{@cells["C4"].render(true)} \n" + "D #{@cells["D1"].render(true)} #{@cells["D2"].render(true)} #{@cells["D3"].render(true)} #{@cells["D4"].render(true)} \n"
+      require 'pry'; binding.pry
     end
-    rows
-    require 'pry'; binding.pry
   end
 end
